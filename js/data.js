d2b29cce50df78365c58bfe2cf989cbf8eac798d
mohamedbaldide-dot <mohamedbaldi.de@gmail.com>
2026-06-09 15:36:05 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,101 +1,158 @@
 const allProducts = [
+    // الصف الأول
     {
         id: 1,
-        title: "Ninja CREAMi Ice Cream Maker & Dessert Machine",
+        title: "Ninja CREAMi Ice Cream Maker",
         image: "images/product1.jpg",
         discount: "29% off",
         price: "USD 203.61",
         oldPrice: "USD 288.45",
+        features: [
+            "2 containers included",
+            "7 programmes",
+            "Ice cream, gelato, sorbet"
+        ],
         link: "#"
     },
     {
         id: 2,
-        title: "Philips OneBlade Original 360 Blades",
+        title: "Philips OneBlade Original",
         image: "images/product2.jpg",
         discount: "38% off",
         price: "USD 34.60",
         oldPrice: "USD 55.37",
+        features: [
+            "Fits all OneBlade models",
+            "4 pack blades",
+            "Original quality"
+        ],
         link: "#"
     },
     {
         id: 3,
-        title: "De'Longhi Magnifica S Coffee Machine",
+        title: "De'Longhi Magnifica S",
         image: "images/product3.jpg",
         discount: "33% off",
         price: "USD 310.39",
         oldPrice: "USD 460.39",
+        features: [
+            "Fully automatic",
+            "Milk frother included",
+            "1.8L water tank"
+        ],
         link: "#"
     },
     {
         id: 4,
-        title: "TOCOL Case for iPhone 12 Magsafe",
+        title: "TOCOL iPhone 12 Case",
         image: "images/product4.jpg",
         discount: "15% off",
         price: "USD 9.80",
         oldPrice: "USD 11.53",
+        features: [
+            "Magsafe compatible",
+            "Full camera protection",
+            "Shockproof"
+        ],
         link: "#"
     },
     {
         id: 5,
-        title: "dé Mobile Wallbox Charging Station",
+        title: "dé Mobile Wallbox",
         image: "images/product5.jpg",
         discount: "15% off",
         price: "USD 166.69",
         oldPrice: "USD 196.10",
+        features: [
+            "11 kW 3-phase",
+            "7m cable",
+            "Adjustable power 6-16A"
+        ],
         link: "#"
     },
     {
         id: 6,
-        title: "Russell Hobbs SatisFry Air Fryer XXL",
+        title: "Russell Hobbs SatisFry",
         image: "images/product6.jpg",
         discount: "57% off",
         price: "USD 64.60",
         oldPrice: "USD 150.34",
+        features: [
+            "XXL 8.3L capacity",
+            "Very quiet operation",
+            "9 programmes"
+        ],
         link: "#"
     },
+    // الصف الثاني
     {
         id: 7,
-        title: "TOCOL iPhone 17 Pro Tempered Glass",
+        title: "TOCOL iPhone 17 Pro Glass",
         image: "images/product7.jpg",
         discount: "15% off",
         price: "USD 9.80",
         oldPrice: "USD 11.53",
+        features: [
+            "Tempered glass 9H",
+            "Full screen coverage",
+            "Anti-fingerprint"
+        ],
         link: "#"
     },
     {
         id: 8,
-        title: "Oral-B iO Ultimate Toothbrush Heads",
+        title: "Oral-B iO Toothbrush Heads",
         image: "images/product8.jpg",
         discount: "46% off",
         price: "USD 43.84",
         oldPrice: "USD 80.76",
+        features: [
+            "8 pack replacement heads",
+            "Ultimate cleaning",
+            "Compatible with iO series"
+        ],
         link: "#"
     },
     {
         id: 9,
-        title: "Fit-Flip Microfibre Travel Towel",
+        title: "Fit-Flip Microfibre Towel",
         image: "images/product9.jpg",
         discount: "15% off",
         price: "USD 19.60",
         oldPrice: "USD 23.07",
+        features: [
+            "Quick-drying",
+            "Compact for travel",
+            "Lightweight"
+        ],
         link: "#"
     },
     {
         id: 10,
-        title: "dreame H12 Pro Vacuum Cleaner",
+        title: "dreame H12 Pro Vacuum",
         image: "images/product10.jpg",
         discount: "26% off",
         price: "USD 229.62",
         oldPrice: "USD 310.39",
+        features: [
+            "Wet and dry cleaning",
+            "180° flat design",
+            "Dries in 5 minutes"
+        ],
         link: "#"
     },
     {
         id: 11,
-        title: "Philips Lumea IPL Hair Removal",
+        title: "Philips Lumea IPL",
         image: "images/product11.jpg",
         discount: "40% off",
         price: "USD 436.15",
         oldPrice: "USD 726.92",
+        features: [
+            "IPL hair removal",
+            "SenseIQ technology",
+            "4 attachments"
+        ],
         link: "#"
     },
     {
@@ -105,6 +162,11 @@ const allProducts = [
         discount: "23% off",
         price: "USD 132.68",
         oldPrice: "USD 173.07",
+        features: [
+            "2.8 inch display",
+            "Infrared night vision",
+            "DEKRA certified"
+        ],
         link: "#"
     }
 ];
