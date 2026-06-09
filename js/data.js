c6c13341381c2fd325242e31bf5690fa30e59993
mohamedbaldide-dot <mohamedbaldi.de@gmail.com>
2026-06-09 15:10:33 +0100

    Update and rename products.js to js/data.js

--- js/data.js
+++ js/data.js
@@ -1,112 +1,112 @@
-const products = [
-    // الصف الأول (6 منتجات)
+const allProducts = [
+    // الصف الأول
     {
         id: 1,
-        title: "Ninja CREAMi Ice Cream Maker & Dessert Machine with 2 Containers, 7 Programmes",
+        title: "Ninja CREAMi Ice Cream Maker & Dessert Machine",
         image: "images/product1.jpg",
         discount: "29% off",
         price: "USD 203.61",
         oldPrice: "USD 288.45",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 2,
-        title: "Philips OneBlade Original 360 Blades Fits All OneBlade and Pro Models",
+        title: "Philips OneBlade Original 360 Blades",
         image: "images/product2.jpg",
         discount: "38% off",
         price: "USD 34.60",
         oldPrice: "USD 55.37",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 3,
-        title: "De'Longhi Magnifica S ECAM 22.110.B fully automatic coffee machine",
+        title: "De'Longhi Magnifica S Coffee Machine",
         image: "images/product3.jpg",
         discount: "33% off",
         price: "USD 310.39",
         oldPrice: "USD 460.39",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 4,
-        title: "TOCOL Case for iPhone 12, Magsafe, Full Camera Protection, Shockproof",
+        title: "TOCOL Case for iPhone 12 Magsafe",
         image: "images/product4.jpg",
         discount: "15% off",
         price: "USD 9.80",
         oldPrice: "USD 11.53",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 5,
-        title: "dé Mobile Wallbox/Charging Station 11 kW [3-Phase | 7 m | 6-16 A]",
+        title: "dé Mobile Wallbox Charging Station",
         image: "images/product5.jpg",
         discount: "15% off",
         price: "USD 166.69",
         oldPrice: "USD 196.10",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 6,
-        title: "Russell Hobbs SatisFry 27632-56 Hot Air Fryer XXL 8.3 L",
+        title: "Russell Hobbs SatisFry Air Fryer XXL",
         image: "images/product6.jpg",
         discount: "57% off",
         price: "USD 64.60",
         oldPrice: "USD 150.34",
-        amazonLink: "#"
+        link: "#"
     },
-    // الصف الثاني (6 منتجات)
+    // الصف الثاني
     {
         id: 7,
-        title: "TOCOL For iPhone 17 Pro Tempered Glass 6.3 Inches, Full Screen",
+        title: "TOCOL iPhone 17 Pro Tempered Glass",
         image: "images/product7.jpg",
         discount: "15% off",
         price: "USD 9.80",
         oldPrice: "USD 11.53",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 8,
-        title: "Oral-B iO Ultimate Cleaning Electric Toothbrush Heads, 8 Pack",
+        title: "Oral-B iO Ultimate Toothbrush Heads",
         image: "images/product8.jpg",
         discount: "46% off",
         price: "USD 43.84",
         oldPrice: "USD 80.76",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 9,
-        title: "Fit-Flip Microfibre Towel, Compact, Ideal as a Sports Towel, Travel Towel",
+        title: "Fit-Flip Microfibre Travel Towel",
         image: "images/product9.jpg",
         discount: "15% off",
         price: "USD 19.60",
         oldPrice: "USD 23.07",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 10,
-        title: "dreame H12 Pro FlexReach Wet and Dry Vacuum Cleaner, Flat 180° Design",
+        title: "dreame H12 Pro Vacuum Cleaner",
         image: "images/product10.jpg",
         discount: "26% off",
         price: "USD 229.62",
         oldPrice: "USD 310.39",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 11,
-        title: "Philips Lumea IPL 9900 Series - IPL Devices Hair Removal",
+        title: "Philips Lumea IPL Hair Removal",
         image: "images/product11.jpg",
         discount: "40% off",
         price: "USD 436.15",
         oldPrice: "USD 726.92",
-        amazonLink: "#"
+        link: "#"
     },
     {
         id: 12,
-        title: "Philips AVENT Baby Monitor with Camera Advanced - DEKRA Certified",
+        title: "Philips AVENT Baby Monitor",
         image: "images/product12.jpg",
         discount: "23% off",
         price: "USD 132.68",
         oldPrice: "USD 173.07",
-        amazonLink: "#"
+        link: "#"
     }
 ];
